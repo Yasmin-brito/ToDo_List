f8b6eea5d470d6cee56fa09493b476824f4a019f
Yasmin-brito <yasmincarneiro2005@gmail.com>
2026-02-16 20:49:26 -0300

    metodos da classe service

---
+++ demo/src/main/java/com/todo_list/demo/service/TaskService.java
@@ -0,0 +1,50 @@
+package com.todo_list.demo.service;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.todo_list.demo.dtos.TaskRecordDto;
+import com.todo_list.demo.model.TaskModel;
+import com.todo_list.demo.repository.TaskRepository;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    public ResponseEntity<TaskModel> save(TaskRecordDto taskRecordDto, TaskModel taskModel){
+        
+        
+        BeanUtils.copyProperties(taskRecordDto, taskModel);
+        //fazer codigo para evitar duplicacao de descrição
+        taskModel.setStatus("Pendente");
+        taskModel.setCreated_at(Instant.now());
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskRepository.save(taskModel));
+    }
+    public ResponseEntity<TaskModel> saveUpdate(TaskModel taskModel){
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskRepository.save(taskModel));
+    }
+
+    public List<TaskModel> allTasks(){
+        return taskRepository.findAll();
+    }
+
+    public Optional<TaskModel> findTaskById(UUID id){
+        return taskRepository.findById(id);
+    }
+
+    public ResponseEntity<Void> deleteTask(TaskModel taskModel){
+        taskRepository.delete(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
