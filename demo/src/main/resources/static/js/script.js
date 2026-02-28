96dba7d1b8e2c1a0e5d0d5efe1b2f3af6a4917c8
Yasmin-brito <yasmincarneiro2005@gmail.com>
2026-02-28 00:20:36 -0300

    desenvolvendo logica de criar, marcar e deletar no js

--- demo/src/main/resources/static/js/script.js
+++ demo/src/main/resources/static/js/script.js
@@ -1,13 +1,13 @@
 const form = document.getElementById("task-form");
-const input = document.getElementById("description");
-const result = document.getElementById("task-body-result-list");
+const descriptionInput = document.getElementById("description");
+const taskList = document.getElementById("task-body-result-list");
 
 addTasktoList();
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
     const task = {
-        description: input.value
+        description: descriptionInput.value
     };
 
     fetch("http://localhost:8080/tasks", {
@@ -20,7 +20,7 @@ form.addEventListener("submit", function(event){
     .then(response => response.json())
     .then(data => {
         addTasktoList();
-        input.value = "";
+        descriptionInput.value = "";
     })
     .catch(error => console.error('Erro: ', error));
 });
@@ -34,17 +34,81 @@ async function addTasktoList() {
         }
 
         const datas = await response.json();
+        
         datas.forEach(element => {
             const li = document.createElement("li");
-            li.textContent = element.description;
+            const checkBox = document.createElement("input")
+            checkBox.type = "checkbox";
+            checkBox.id = `checkbox-${element.id}`;
 
-            //marcar como concluida
-            if(element.completed == "concluido") {
-                li.style.textDecoration = "line-through";
+            const label = document.createElement("label");
+            label.setAttribute("for", checkBox.id);
+
+            const span = document.createElement("span");
+            span.textContent = element.description;
+
+            const btnIconPen = document.createElement("button");
+            const btnIconTrash = document.createElement("button");
+            btnIconTrash.innerHTML = '<i class="fi fi-rr-trash-xmark"></i>'
+            btnIconPen.innerHTML = '<i class="fi fi-rr-pencil"></i>'
+            btnIconPen.className = "btn-icon-pen";
+            btnIconTrash.className = "btn-icon-trash";
+
+            btnIconTrash.addEventListener("click", async () =>{
+                const confirmDelete = confirm("Deseja realmente excluir esta task?");
+                if (!confirmDelete) return;
+
+                try {
+                    await fetch(`http://localhost:8080/tasks/${element.id}`, {
+                        method: "DELETE"
+                    });
+                    li.remove();
+
+                } catch (error) {
+                    console.error("Erro ao deletar:", error);
+                }
+
+            })
+            
+            label.appendChild(span)
+            li.appendChild(checkBox);
+            li.appendChild(label);
+            li.appendChild(btnIconPen)
+            li.appendChild(btnIconTrash)
+
+            if(element.status == "CONCLUIDO") {
+                checkBox.checked = true;
             }
-            result.appendChild(li);
+            Boxchecked(checkBox, li, element, span);
+            taskList.appendChild(li);
         });
    } catch (error){
     console.error('Erro: ', error);
    }
+}
+function Boxchecked(checkBox, li, element, span){
+    checkBox.addEventListener("change", async () => {
+        const statusCheckBox = checkBox.checked ? "CONCLUIDA" : "PENDENTE";
+        span.style.textDecoration = checkBox.checked ? "line-through" : "none";
+        try {
+            await fetch(`http://localhost:8080/tasks/${element.id}/status`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    status: statusCheckBox
+                })
+            });
+
+        }catch (error) {
+            console.error("Erro:", error);
+        }
+    });
+}
+
+function clickIconPen() {
+    btnPen.addEventListener("click", async () => {
+
+    })
 }
